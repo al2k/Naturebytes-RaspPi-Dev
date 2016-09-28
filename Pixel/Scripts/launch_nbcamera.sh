ef86cf5038375df1f32ec177df1c34d2c7e6193a
Alasdair Davies <a.davies@handshakeproductions.org>
2016-09-28 23:04:43 +0000

    Launch the nbcamera.py script on boot

---
+++ Pixel/Scripts/launch_nbcamera.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# launch_nbcamera.sh
+# navigate to home directory, then to this directory, then execute python script, then back home
+
+cd /
+cd home/pi/Naturebytes/Scripts/
+python nbcamera.py
+cd /
