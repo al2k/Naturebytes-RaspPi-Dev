83b8f320219a3255503fdd4180602c2ef414dba4
Claire Gurman <claire@raffles.me.uk>
2017-01-08 12:36:23 +0000

    Add save location command line option

--- Pixel/Scripts/nbcamera.py
+++ Pixel/Scripts/nbcamera.py
@@ -1,12 +1,17 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # Naturebytes Wildlife Cam Kit | V1.07 (Pixel)
 # Based on the excellent official Raspberry Pi tutorials and a little extra from Naturebytes
+#
+# Useage: nbcamera.py -o <outputlocation>
+# ======================================================================
 
 import RPi.GPIO as GPIO
 import time
 from subprocess import call
 from datetime import datetime
 import logging
+import getopt
+import sys
 
 # Logging all of the camera's activity to the "naturebytes_camera_log" file. If you want to watch what your camera
 # is doing step by step you can open a Terminal window and type "cd /Naturebytes/Scripts" and then type
@@ -27,91 +32,109 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(sensorPin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 # GPIO.setup(lowbattPin, GPIO.IN)
 
-# Defining our default states so we can detect a change
+# Set default save location
+saveLocation = "/media/usb0/"
 
-prevState = False
-currState = False
-# prevBattState = False
-# currBattState = False
+def main(argv):
+    # Set save location
+    try:
+        opts, args = getopt.getopt(argv, "ho:")
+        for opt, arg in opts:
+            if opt == "-h":
+                print "nbcamera.py -o <outputlocation>"
+                sys.exit()
+            elif opt == "-o":
+                saveLocation = arg
+    except getopt.GetoptError:
+        print "nbcamera.py -o <outputlocation>"
+        sys.exit(2)
 
-# Starting a loop
+    # Defining our default states so we can detect a change
 
-while True:
-    time.sleep(0.1)
-    prevState = currState
-    # prevBattState = currBattState
+    prevState = False
+    currState = False
+    # prevBattState = False
+    # currBattState = False
 
-    # Map the state of the camera to our input pins (jumper cables connected to your PIR)
+    # Starting a loop
 
-    currState = GPIO.input(sensorPin)
-    # currBattState = GPIO.input(lowbattPin)
+    while True:
+        time.sleep(0.1)
+        prevState = currState
+        # prevBattState = currBattState
 
-    # Checking that our state has changed
+        # Map the state of the camera to our input pins (jumper cables connected to your PIR)
 
-    if currState != prevState:
-    # About to check if our new state is HIGH or LOW
+        currState = GPIO.input(sensorPin)
+        # currBattState = GPIO.input(lowbattPin)
 
-        newState = "HIGH" if currState else "LOW"
-        #  newBattState = "HIGH" if currBattState else "LOW"
-        print "GPIO pin %s is %s" % (sensorPin, newState)
-        # print "Battery level detected via pin %s is %s" % (lowbattPin, newBattState)
+        # Checking that our state has changed
 
-        if currState:  # Our state has changed, so that must be a trigger from the PIR
+        if currState != prevState:
+        # About to check if our new state is HIGH or LOW
 
-            i = datetime.now() # Get the time now
-            get_date = i.strftime('%Y-%m-%d') # Get and format the date
-            get_time = i.strftime('%H-%M-%S.%f') # Get and format the time
-            # batt_state = newBattState
-            # Checking the current status of the battery
+            newState = "HIGH" if currState else "LOW"
+            #  newBattState = "HIGH" if currBattState else "LOW"
+            print "GPIO pin %s is %s" % (sensorPin, newState)
+            # print "Battery level detected via pin %s is %s" % (lowbattPin, newBattState)
 
-            # Recording that a PIR trigger was detected and logging the battery level at this time
-            logging.info('PIR trigger detected')
-            # logging.info('Battery level is %(get_batt_level)s', { 'get_batt_level': batt_state })
+            if currState:  # Our state has changed, so that must be a trigger from the PIR
 
-            # Assigning a variable so we can create a photo JPG file that contains the date and time as its name
-            photo = get_date + '_' +  get_time + '.jpg'
+                i = datetime.now() # Get the time now
+                get_date = i.strftime('%Y-%m-%d') # Get and format the date
+                get_time = i.strftime('%H-%M-%S.%f') # Get and format the time
+                # batt_state = newBattState
+                # Checking the current status of the battery
 
-            # Using the raspistill library to take a photo. You can show that a photo has been taken in a small preview box on the desktop by changing --nopreview to --preview
-            cmd = 'raspistill -t 300 -w 1920 -h 1440 --nopreview -o /media/usb0/' + photo
-        print 'cmd ' +cmd
+                # Recording that a PIR trigger was detected and logging the battery level at this time
+                logging.info('PIR trigger detected')
+                # logging.info('Battery level is %(get_batt_level)s', { 'get_batt_level': batt_state })
 
-            # If you find you have permission problems saving to other attached non-Naturebytes storage devices you can use this line to change the owner of the photo if required
-            # perms = 'chown pi:pi /media/usb0/' + photo
-            # print 'perms ' +perms
+                # Assigning a variable so we can create a photo JPG file that contains the date and time as its name
+                photo = get_date + '_' +  get_time + '.jpg'
 
-            # Log that we have just taking a photo"
-            logging.info('About to take a photo and save to the USB drive')
-            call ([cmd], shell=True)
-            # call ([perms], shell=True)
+                # Using the raspistill library to take a photo. You can show that a photo has been taken in a small preview box on the desktop by changing --nopreview to --preview
+                cmd = 'raspistill -t 300 -w 1920 -h 1440 --nopreview -o ' + photo
+                print 'cmd ' +cmd
 
-            # Log that a photo was taken successfully and state the file name so we know which one"
-            logging.info('Photo taken successfully %(show_photo_name)s', { 'show_photo_name': photo })
-            photo_location =  '/media/usb0/' + photo
+                # If you find you have permission problems saving to other attached non-Naturebytes storage devices you can use this line to change the owner of the photo if required
+                # perms = 'chown pi:pi /media/usb0/' + photo
+                # print 'perms ' +perms
 
-            # Log that we are about to attempt to write the overlay text. This was removed in v1.07 to speed up the capture process."
-            logging.info('Skipping legacy overlay text')
+                # Log that we have just taking a photo"
+                logging.info('About to take a photo and save to the USB drive')
+                call ([cmd], shell=True)
+                # call ([perms], shell=True)
 
-            # overlay = "/usr/bin/convert "+ photo_location + " "
+                # Log that a photo was taken successfully and state the file name so we know which one"
+                logging.info('Photo taken successfully %(show_photo_name)s', { 'show_photo_name': photo })
+                photo_location =  '/media/usb0/' + photo
 
-            # Use ImageMagick to write text and meta data onto the photo.
-            # overlay += " -gravity north -background black -extent +0+40 +repage -box black -fill white -pointsize 24 -gravity southwest -annotate +6+6 'Naturebytes Wildlife Cam Kit | Date & Time: " + get_date + '" '" + get_time '" -gravity southeast -annotate +6+6 'Camera 1 " "'" + photo_location
-            # overlay += " -gravity north -background black -extent +0+40 +repage -box black -fill white -pointsize 24 -gravity southwest -annotate +6+6 'Naturebytes Wildlife Cam Kit | Date & Time: " + get_date + " " + get_time + "' -gravity southeast -annotate +6+6 'Camera 1' " + photo_location
+                # Log that we are about to attempt to write the overlay text. This was removed in v1.07 to speed up the capture process."
+                logging.info('Skipping legacy overlay text')
 
-            # Log that we the text was added successfully"
-            # logging.info('Added the overlay text successfully')
-            # call ([overlay], shell=True)
+                # overlay = "/usr/bin/convert "+ photo_location + " "
 
-            # Add a small Naturebytes logo to the top left of the photo. Note - you could change this to your own logo if you wanted.
-            # logging.info('Adding the Naturebytes logo')
-            # overlay = '/usr/bin/convert '+ photo_location + ' /home/pi/Naturebytes/Scripts/naturebytes_logo_80.png -geometry +1+1 -composite ' + photo_location
-            # call ([overlay], shell=True)
+                # Use ImageMagick to write text and meta data onto the photo.
+                # overlay += " -gravity north -background black -extent +0+40 +repage -box black -fill white -pointsize 24 -gravity southwest -annotate +6+6 'Naturebytes Wildlife Cam Kit | Date & Time: " + get_date + '" '" + get_time '" -gravity southeast -annotate +6+6 'Camera 1 " "'" + photo_location
+                # overlay += " -gravity north -background black -extent +0+40 +repage -box black -fill white -pointsize 24 -gravity southwest -annotate +6+6 'Naturebytes Wildlife Cam Kit | Date & Time: " + get_date + " " + get_time + "' -gravity southeast -annotate +6+6 'Camera 1' " + photo_location
 
-            # Log that the logo was added succesfully"
-            # logging.info('Logo added successfully')
+                # Log that we the text was added successfully"
+                # logging.info('Added the overlay text successfully')
+                # call ([overlay], shell=True)
 
-        else:
+                # Add a small Naturebytes logo to the top left of the photo. Note - you could change this to your own logo if you wanted.
+                # logging.info('Adding the Naturebytes logo')
+                # overlay = '/usr/bin/convert '+ photo_location + ' /home/pi/Naturebytes/Scripts/naturebytes_logo_80.png -geometry +1+1 -composite ' + photo_location
+                # call ([overlay], shell=True)
 
-           # print "Waiting for a new PIR trigger to continue"
-           logging.info('Waiting for a new PIR trigger to continue')
+                # Log that the logo was added succesfully"
+                # logging.info('Logo added successfully')
 
-# END
+            else:
+
+               # print "Waiting for a new PIR trigger to continue"
+               logging.info('Waiting for a new PIR trigger to continue')
+
+if __name__ == "main":
+    main(sys.argv[1:])
