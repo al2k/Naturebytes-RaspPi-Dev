ab5f6420cd52ac1f5c5f31fde440b05df138b0dd
CrabbyPete <86552+CrabbyPete@users.noreply.github.com>
2024-12-21 08:17:01 -0500

    main does video

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python
 # Naturebytes Wildlife Cam Kit | V1.01
 # Based on the excellent official Raspberry Pi tutorials and a little extra from Naturebytes
+import os
 import sys
 import csv
 import time
 import arrow
-import pathlib
 import logging
 import argparse
 
 import RPi.GPIO as GPIO
 
-from subprocess import call
+from subprocess import run
 from threading import Thread
 
 # Logging all of the camera's activity to the "naturebytes_camera_log" file. If you want to watch what your camera
@@ -30,7 +30,7 @@ GPIO.setup(SENSOR_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(BATTERY_PIN, GPIO.IN)
 
 def what_os():
-    path = pathlib.Path("/etc/os-release")
+    path = "/etc/os-release"
     with open(path) as stream:
         reader = csv.reader(stream, delimiter="=")
         os_release = dict(reader)
@@ -53,16 +53,18 @@ def take_photo(command, save_to, use_overlay):
     photo = now +'.jpg'
 
     # Using the raspistill library to take a photo and show that a photo has been taken in a small preview box on the desktop
-    cmd = f'{command} --output {photo}'
+    cmd = f'{command} -o {photo}'
     logging.info(f"cmd:{cmd}")
 
     # Log that we have just taking a photo"
-    logging.info(f'About to take a photo {photo}')
-    call(cmd.split(), shell=True)
+    cmd = cmd.split()
+    run(cmd, shell=True)
 
     # Log that a photo was taken successfully and state the file name so we know which one"
     logging.info('Photo taken successfully %(show_photo_name)s', {'show_photo_name': photo})
-    photo_location =  save_to + photo
+    photo_location =  os.path.join(save_to,photo)
+    if os.path.exists(photo):
+        print(os.path.abspath(photo))
 
     if use_overlay:
         # Log that we are about to attempt to write the overlay text"
@@ -76,29 +78,23 @@ def take_photo(command, save_to, use_overlay):
 
         # Log that we the text was added successfully"
         logging.info('Added the overlay text successfully')
-        call([overlay], shell=True)
+        run(overlay.split, shell=True, check=True)
 
         # Add a small Naturebytes logo to the top left of the photo. Note - you could change this to your own logo if you wanted.
         logging.info('Adding the Naturebytes logo')
         overlay = '/usr/bin/convert ' + photo_location + ' ./naturebytes_logo_80.png -geometry +1+1 -composite ' + photo_location
-        call([overlay], shell=True)
+        run(overlay.split(), shell=True)
 
         # Log that the logo was added successfully"
         logging.info('Logo added successfully')
     else:
-        call(["mv",f"{photo}",f"{save_to}"])
+        run(["mv",f"{photo}",f"{save_to}"])
 
 
-def main(save_to, use_overlay, video):
-    """
-    Main loop
-    :param save_to: str: where to save pictures
-    :param use_overlay: bool: add the Naturebytes logo
-    :param video: bool: take video instead of stills
-    :return: Never
-    """
+def main(save_to='./', use_overlay=False, video=False):
+    prev_state = curr_state = 0
+
     print(f"Saving photos to {save_to}")
-
     # Starting with Bookworm the cammand name changed
     os_release = what_os()
     version = os_release.get('VERSION')
@@ -108,22 +104,30 @@ def main(save_to, use_overlay, video):
         cam_command = 'libcamera-still'
 
     while True:
+        time.sleep(0.1)
+        prev_state = curr_state
 
         # Map the state of the camera to our input pins (jumper cables connected to your PIR)
         curr_state = GPIO.input(SENSOR_PIN)
-        if curr_state:
+        if not curr_state == prev_state:
             # About to check if our new state is HIGH or LOW
-            logging.info(f"GPIO {'HIGH' if curr_state else 'LOW'}")
 
-            # task = Thread(target=take_photo, args=[cam_command, save_to, use_overlay])
-            # task.run()
-            take_photo(cam_command, save_to, use_overlay)
-            time.sleep(30)
+            state = "HIGH" if curr_state else "LOW"
+            logging.info(f"GPIO {SENSOR_PIN} pin:{state}")
+            # print "Battery level detected via pin %s is %s" % (lowbattPin, newBattState)
+
+            if curr_state:  # Our state has changed, so that must be a trigger from the PIR
+                task = Thread(target=take_photo, args=[cam_command, save_to, use_overlay])
+                task.run()
+            else:
+                # print "Waiting for a new PIR trigger to continue"
+                logging.info('Waiting for a new PIR trigger to continue')
+
 
 if __name__ == "__main__":
     import argparse
     args = argparse.ArgumentParser( prog='Capture camera images')
-    save_to = '/home/pi/naturebytes/Naturebytes-RaspPi-Dev/static/photos'
+    save_to = './'
     overlay = True
     video = False
 
