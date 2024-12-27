c67741ecbd43e4f59bac0f3e463e6ce577e6f332
CrabbyPete <86552+CrabbyPete@users.noreply.github.com>
2024-12-27 10:47:38 -0500

    fix up photo taking

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
 sudo apt update
 sudo apt upgrade
-sudo apt -y install python3-rpi.gpio python3-flask git imagemagic
+sudo apt -y install python3-rpi.gpio python3-flask git imagemagick python3-opencv python3-arrow python3-picamera2
 
+git clone https://github.com/naturebytes/Naturebytes-RaspPi-Dev.git
 cp *.py /usr/local/src
 cp -r static /usr/local/src
 cp -r templates /usr/local/src
